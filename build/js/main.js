0b1eec3c787a68d30a7769595a13002aad11bb98
KaterinkaVasilek <e.kushner@bk.ru>
2022-07-21 14:37:19 +0300

    fix: css

---
+++ build/js/main.js
@@ -0,0 +1,40 @@
+// const personalPlanPeter = {
+//   name: "Peter",
+//   age: "29",
+//   skills: {
+//       languages: ['ru', 'eng'],
+//       programmingLangs: {
+//           js: '20%',
+//           php: '10%'
+//       },
+//       exp: '1 month'
+//   }
+// };
+
+// function showExperience(plan) {
+//   const {exp} = personalPlanPeter.skills;
+//   return exp;
+// }
+
+// function getKey (plan) {
+//   for (let key in plan) {
+
+//   }
+// }
+
+// getKey(personalPlanPeter);
+
+// // function showProgrammingLangs(plan) {
+// //   let str = '';
+// //   let {programmingLangs} = personalPlanPeter.skills;
+// //   // const {js, php} = personalPlanPeter.skills.programmingLangs;
+// //   if (programmingLangs = {}) {
+// //     return str;
+// //   } else {
+    
+// //   }
+// // }
+
+// console.log(showExperience(personalPlanPeter));
+// // console.log(showProgrammingLangs(personalPlanPeter));
+
